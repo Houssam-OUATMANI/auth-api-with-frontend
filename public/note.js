108987860f6c5fc4be5185ebd5a0643f1dbc33d7
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2020-12-15 14:34:06 +0100

    Auth API

---
+++ public/note.js
@@ -0,0 +1,28 @@
+console.log(`
+    Houssam OUATMANI
+    JavaScript Web and Mobile developper 
+    HTML
+    CSS
+    React
+    Vue
+    Node
+    MongoDb
+    MySQL
+
+    email : ouatmanihoussam@gmail.com
+`)
+
+
+alert(`
+Houssam OUATMANI
+JavaScript Web and Mobile developper 
+HTML
+CSS
+React
+Vue
+Node
+MongoDb
+MySQL
+
+email : ouatmanihoussam@gmail.com
+`)
